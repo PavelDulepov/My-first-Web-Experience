d90a9840eb3a9a90239b9af6892d01cf10089cf7
PavelDulepov <45333089+PavelDulepov@users.noreply.github.com>
2018-12-01 20:43:41 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -20,31 +20,83 @@ function ListShow ()  {
  valueChange.onclick=fak;
  
 document.getElementsByClassName('plusButton').onclick=function(){
-    document.getElementsByClassName('change').innerHTML='4';
+    document.getElementsByClassName('change').innerHTML()='4';
 }
 } 
 $(document).ready(function(){
+    var holder;
     
       $('.plusButton').click(function(){ 
+        
           console.log('Mazahaka Jquery Sucks')
           var holder=$(this).parent().parent().find('.innerValue').text();
           holder++;
-
+         
+          if(holder>='1'&&$(this).parent().parent().hasClass('value1Cookies')){
+                $('.statusBlock2').css('display',"inline-block");
+            }
+            else if(holder>='1'&&$(this).parent().parent().hasClass('value1Tomat')){
+                $('.statusBlock4').css('display','inline-block');
+            }
+            else if(holder>='1'&&$(this).parent().parent().hasClass('value1Cheese')){
+                $('.statusBlock3').css('display','inline-block');
+            }
+            
           console.log(holder);
+          if($(this).parent().parent().hasClass("value1Tomat")){
+            console.log('suka');
+            $("#OrangeCircleTomatValue").text(holder);
+            } 
+            else if($(this).parent().parent().hasClass("value1Cookies")){
+            $('#OrangeCircleCookiesValue').text(holder);
+            }
+            else if($(this).parent().parent().hasClass("value1Cheese")){
+                $('#OrangeCircleCheeseValue').text(holder);
+                }
+            
         $(this).parent().parent().find('.innerValue').html(holder);  
         }); 
         $('.minusButton').click(function(){ 
-             console.log('Mazahaka Jquery Fucks');  
+             console.log('Mazahaka Jquery Fucks'); 
+            //  console.log(holder);
+             
              var holder=$(this).parent().parent().find('.innerValue').text();
             
 
-            console.log(holder);
+           
+            
+            
+            
             if(holder==0){
                 return;
             }
             holder--;
+            if($(this).parent().parent().hasClass("value1Tomat")){
+                console.log('suka');
+                $("#OrangeCircleTomatValue").text(holder);
+                
+            } 
+            else if($(this).parent().parent().hasClass("value1Cookies")){
+                    $('#OrangeCircleCookiesValue').text(holder);
+            }
+            else if($(this).parent().parent().hasClass("value1Cheese")){
+                $('#OrangeCircleCheeseValue').text(holder);
+            }
+            if(holder<'1'&&$(this).parent().parent().hasClass('value1Cookies')){
+                console.log('work');
+                $('.statusBlock2').css('display',"none");
+            }
+            else if(holder<'1'&&$(this).parent().parent().hasClass('value1Tomat')){
+                $('.statusBlock4').css('display','none');
+            }
+            else if(holder<'1'&&$(this).parent().parent().hasClass('value1Cheese')){
+                $('.statusBlock3').css('display','none');
+            }
         $(this).parent().parent().find('.innerValue').html(holder);
             });  
             $('button').mouseenter(function (){  console.log('tooltip'); 
-         });  
+         });
+           
+         
         });
+
